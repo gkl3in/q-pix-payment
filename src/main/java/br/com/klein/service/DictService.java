279003bc58537f9346c1ee2901b8c0bdbcdc8206
Gabriel Klein <gabrielklein289@hotmail.com>
2025-02-14 12:37:24 -0300

    feat: added cache on dictService

--- src/main/java/br/com/klein/service/DictService.java
+++ src/main/java/br/com/klein/service/DictService.java
@@ -3,11 +3,16 @@ package br.com.klein.service;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import com.oracle.svm.core.annotate.Inject;
+
+import br.com.klein.config.RedisCache;
 import br.com.klein.model.Chave;
 import br.com.klein.model.TipoChave;
 import br.com.klein.model.TipoPessoa;
+import io.quarkus.logging.Log;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @ApplicationScoped
 public class DictService {
@@ -20,6 +25,28 @@ public class DictService {
     private String cnpj;
     @ConfigProperty(name = "pix.nome")
     private String nome;
+
+    @Inject
+    RedisCache redisCache;
+
+    public Chave buscarDetalhesChave(String key) {
+        Chave chave = buscarChaveCache(key);
+
+        if (Objects.isNull(chave)) {
+            var chaveFake = buscarChave(key);
+            redisCache.set(key, buscarChave(key));
+            return chaveFake;
+        }
+
+        return chave;
+    }
+
+    private Chave buscarChaveCache(String key) {
+        Chave chave = redisCache.get(key).get();
+        Log.infof("Chave encontrada no cache %s", chave);
+
+        return chave;
+    }
 
     public Chave buscarChave(String chave) {
         return new Chave(TipoChave.CPF,

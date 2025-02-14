4cd2e29b075381b8547b08c99d63e8a478349a64
Gabriel Klein <gabrielklein289@hotmail.com>
2025-02-14 12:21:34 -0300

    feat: Added redis to the project and created redisConfig

--- src/main/java/br/com/klein/model/Chave.java
+++ src/main/java/br/com/klein/model/Chave.java
@@ -2,7 +2,10 @@ package br.com.klein.model;
 
 import java.time.LocalDateTime;
 
-public record Chave(TipoChave tipoChave, String chave, String ispb, TipoPessoa tipoPessoa,
+public record Chave(TipoChave tipoChave, 
+                    String chave, 
+                    String ispb, 
+                    TipoPessoa tipoPessoa,
                     String cpfCnpj,
                     String nome,
                     LocalDateTime dataHoraCriacao

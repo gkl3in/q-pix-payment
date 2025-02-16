be2bfedc7067bbd181a5678fc1543be44461d332
Gabriel Klein <gabrielklein289@hotmail.com>
2025-02-16 08:56:02 -0300

    feat: implemented documentation on models LinhaDigitavel and Pix

--- src/main/java/br/com/klein/model/Pix.java
+++ src/main/java/br/com/klein/model/Pix.java
@@ -1,6 +1,11 @@
 package br.com.klein.model;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 import java.math.BigDecimal;
 
-public record Pix(String chave, BigDecimal valor, String cidadeRemetente) {
+public record Pix(
+        @Schema(description = "Chave cadastrada do recebedor") String chave,
+        @Schema(description = "Valor da transação") BigDecimal valor,
+        @Schema(description = "Cidade remetente do recebedor") String cidadeRemetente) {
 }
